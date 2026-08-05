91917f5499e6ebdc09837e2e80bfcb1ef0a78ca1
Adhyan Tyagi <adhyan.tyagi.28gmail.com>
2026-08-05 23:41:10 +0530

    Add resume upload page

--- main.py
+++ main.py
@@ -5,7 +5,20 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
+
     return render_template("index.html")
+
+
+@app.route("/upload")
+def upload():
+
+    return render_template("upload.html")
+
+
+@app.route("/about")
+def about():
+
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
